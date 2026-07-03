740f7c765aee21b6e4d63eb53a828606eda235a3
junseok816 <junseok816@gmail.com>
2026-07-03 16:15:57 +0900

    feat: git 버튼 추가

--- front-end/src/component/Editor/TitleBar.tsx
+++ front-end/src/component/Editor/TitleBar.tsx
@@ -13,7 +13,19 @@ const TitleBar: React.FC<TitleBarProps> = ({ editor }) => {
       <div className="title-bar__brand">
         <span className="brand-indicator" />
         Toon Studio
-      </div>      
+      </div>
+      <div className="title-bar__actions">
+        <a
+          className="title-bar__github"
+          href="https://github.com/jun-seok816/webtoon"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="GitHub 저장소 열기"
+          title="GitHub 저장소"
+        >
+          <i className="bi bi-github" aria-hidden="true" />
+        </a>
+      </div>
     </header>
   );
 };
